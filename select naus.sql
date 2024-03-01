e3b1aea9877cc4f509c47f1bbcae2b6989c67bd4
R3TR0R0C4 <nicoroca04@gmail.com>
2024-03-01 10:59:34 +0100

    Update select naus.sql

--- select naus.sql
+++ select naus.sql
@@ -1,8 +1,9 @@
-select naus.nom, tripulants.nom
+/*select naus.nom, tripulants.nom
 from naus 
 join tripulants on naus.nom = tripulants.nom
 join (select nom, MIN(pes) as pesMinim max(pes) as pesMaxim
     from naus
     where viatjants.codi_data between '2024-04-01' and '2024-04-2'
     group by naus.nom) on naus.nom=tripulants.nom
-where naus.pes =naus.pesMinim or naus.pes = pesMaxim
+where naus.pes =naus.pesMinim or naus.pes = pesMaxim*/
+
